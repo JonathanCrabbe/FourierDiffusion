852a9c4bf630597d09a4a55d3825c281d9714a24
JonathanCrabbe <jonathan.cr1302@gmail.com>
2023-11-13 14:49:26 +0000

    Add timesteps to DiffusableBatch class and collate_batch function.

--- fdiff/utils/dataclasses.py
+++ fdiff/utils/dataclasses.py
@@ -8,6 +8,14 @@ import torch
 class DiffusableBatch:
     X: torch.Tensor
     y: Optional[torch.Tensor] = None
+    timesteps: Optional[torch.Tensor] = None
+
+    def __len__(self) -> int:
+        return len(self.X)
+
+    @property
+    def device(self) -> torch.device:
+        return self.X.device
 
 
 def collate_batch(data: list[dict[str, torch.Tensor]]) -> DiffusableBatch:
@@ -15,5 +23,9 @@ def collate_batch(data: list[dict[str, torch.Tensor]]) -> DiffusableBatch:
 
     X = torch.stack([example["X"] for example in data])
     y = torch.stack([example["y"] for example in data]) if "y" in data[0] else None
-
-    return DiffusableBatch(X=X, y=y)
+    timesteps = (
+        torch.stack([example["timestep"] for example in data])
+        if "timestep" in data[0]
+        else None
+    )
+    return DiffusableBatch(X=X, y=y, timesteps=timesteps)
