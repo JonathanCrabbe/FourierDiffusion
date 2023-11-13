1ffccff7c8f21e9c03ca7713887605e6d19f46f5
JonathanCrabbe <jonathan.cr1302@gmail.com>
2023-11-13 11:33:31 +0000

    Add Positional and Time Encoding to Transformer Model

--- fdiff/models/transformer.py
+++ fdiff/models/transformer.py
@@ -18,16 +18,41 @@ class PositionalEncoding(nn.Module):
         )  # (max_len, d_emb)
 
         # Positional encodings broadcasted accross batch dimension
-        pe = self.embedding(position)  # (1, max_len, d_emb)
-        self.pe = rearrange(pe, "b l d -> b d l")  # (1, d_emb, max_len)
+        self.pe = self.embedding(position)  # (1, max_len, d_emb)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Arguments:
-            x: Tensor, shape ``[batch_size, d_embed, max_len]``
+        """Adds a positional encoding to the tensor x.
+
+        Args:
+            x (torch.Tensor): Tensor of shape (batch_size, max_len, d_emb) to which the positional encoding should be added
 
         Returns:
-            x: Tensor, shape ``[batch_size, d_embed, max_len]``
+            torch.Tensor: Tensor with an additional positional encoding
         """
         x = x + self.pe
         return x
+
+
+class TimeEncoding(nn.Module):
+    def __init__(self, d_model: int, max_time: int):
+        super().__init__()
+
+        # Learnable Embedding matrix to map time steps to embeddings
+        self.embedding = nn.Embedding(
+            num_embeddings=max_time, embedding_dim=d_model, max_norm=math.sqrt(d_model)
+        )  # (max_time, d_emb)
+
+    def forward(self, x: torch.Tensor, timesteps: torch.LongTensor) -> torch.Tensor:
+        """Adds a time encoding to the tensor x.
+
+        Args:
+            x (torch.Tensor): Tensor of shape (batch_size, max_len, d_emb) to which the time encoding should be added
+            timesteps (torch.LongTensor): Tensor of shape (batch_size,) containing the current timestep for each sample in the batch
+
+        Returns:
+            torch.Tensor: Tensor with an additional time encoding
+        """
+        t_emb = self.embedding(timesteps)  # (batch_size, d_emb)
+        t_emb = t_emb.unsqueeze(1)  # (batch_size, 1, d_emb)
+        assert isinstance(t_emb, torch.Tensor)
+        return x + t_emb
--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -1,29 +1,53 @@
 import torch
 
-from fdiff.models.transformer import PositionalEncoding
+from fdiff.models.transformer import PositionalEncoding, TimeEncoding
 
-max_len = 20
-batch_size = 16
-d_model = 5
-EPS = 1e-5
+max_len = 20  # maximum time series length
+max_time = 100  # maximum diffusion time step
+batch_size = 16  # number of time series in a batch
+d_model = 5  # embedding dimension
+EPS = 1e-5  # tolerance for floating point errors
 
 
 def test_positional_encoding():
     torch.manual_seed(42)
 
     pos_encoder = PositionalEncoding(d_model=d_model, max_len=max_len)
-    X = torch.randn((batch_size, d_model, max_len))
+    X = torch.randn((batch_size, max_len, d_model))
     enc_out = pos_encoder(X)
 
     # Check that the output has consistent shape
     assert enc_out.shape == X.shape
 
     # Check that the max_norm constrtaint for positional encoding is satisfied
-    assert torch.max(torch.sum((enc_out - X) ** 2, dim=1)) <= d_model + EPS
+    assert torch.max(torch.sum((enc_out - X) ** 2, dim=-1)) <= d_model + EPS
 
-    # Check that time step is assigned the right emconding vector
+    # Check that each time step is assigned the right encoding vector
     for b in range(batch_size):
         for l in range(max_len):
             assert torch.allclose(
-                (enc_out - X)[b, :, l], pos_encoder.pe[0, :, l], atol=EPS
+                (enc_out - X)[b, l, :], pos_encoder.pe[0, l, :], atol=EPS
+            )
+
+
+def test_time_encoding():
+    torch.manual_seed(42)
+
+    time_encoder = TimeEncoding(d_model=d_model, max_time=max_time)
+    X = torch.randn((batch_size, max_len, d_model))
+    timesteps = torch.randint(low=0, high=max_len, size=(batch_size,))
+
+    enc_out = time_encoder(X, timesteps)
+
+    # Check that the output has consistent shape
+    assert enc_out.shape == X.shape
+
+    # Check that the max_norm constrtaint for time encoding is satisfied
+    assert torch.max(torch.sum((enc_out - X) ** 2, dim=-1)) <= d_model + EPS
+
+    # Check that each batch element is assigned the right encoding vector
+    for b in range(batch_size):
+        for l in range(max_len):
+            assert torch.allclose(
+                (enc_out - X)[b, l, :], time_encoder.embedding(timesteps[b]), atol=EPS
             )
