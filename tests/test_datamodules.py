63efc2e4e974f7bff046b34e24075e3460b86059
JonathanCrabbe <jonathan.cr1302@gmail.com>
2023-11-13 15:34:18 +0000

    Add variables for max_len and n_channels in DummyDatamodule constructor

--- tests/test_datamodules.py
+++ tests/test_datamodules.py
@@ -17,18 +17,23 @@ class DummyDatamodule(Datamodule):
         self,
         data_dir: Path = Path.cwd() / "data",
         random_seed: int = 42,
-        batch_size: int = 32,
+        batch_size: int = batch_size,
+        max_len: int = max_len,
+        n_channels: int = n_channels,
     ) -> None:
         super().__init__(
             data_dir=data_dir, random_seed=random_seed, batch_size=batch_size
         )
+        self.max_len = max_len
+        self.n_channels = n_channels
+        self.batch_size = batch_size
 
     def prepare_data(self) -> None:
         self.X_train = torch.randn(
-            (10 * batch_size, max_len, n_channels), dtype=torch.float32
+            (10 * self.batch_size, self.max_len, self.n_channels), dtype=torch.float32
         )
         self.y_train = torch.randint(
-            low=low, high=high, size=(10 * batch_size,), dtype=torch.long
+            low=low, high=high, size=(10 * self.batch_size,), dtype=torch.long
         )
         self.X_test = torch.randn_like(self.X_train)
         self.y_test = torch.randint_like(self.y_train, low=low, high=high)
