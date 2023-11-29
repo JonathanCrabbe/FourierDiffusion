86f271df84261e273b01f0b00fd8594bbd3ea734
JonathanCrabbe <jonathan.cr1302@gmail.com>
2023-11-29 11:06:45 +0000

    Refactor test_metrics.py to use pot estimation of sliced Wasserstein

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,13 +1,17 @@
 import numpy as np
 import pytest
+from ot.sliced import sliced_wasserstein_distance
 
 from fdiff.sampling.metrics import MarginalWasserstein, SlicedWasserstein
+from fdiff.utils.tensors import check_flat_array
 
 random_seed = 42
-n_dim = 2
-n_samples = 10000
+n_channels = 1
+max_len = 2
+n_samples = 1000
+num_directions = 1000
 
-test_data_wasserstein = [0.0, 0.1, 1.0, 5.0]
+test_data_wasserstein = [0.0, 0.1, 1.0]
 
 
 @pytest.mark.parametrize("shift", test_data_wasserstein)
@@ -16,20 +20,26 @@ def test_sliced_waserstein(shift: float):
     np.random.seed(random_seed)
 
     # Initialize two datasets
-    dataset1 = np.random.rand(n_samples, n_dim)
-    dataset2 = np.random.rand(n_samples, n_dim) + shift
+    dataset1 = np.random.rand(n_samples, max_len, n_channels)
+    dataset2 = np.random.rand(n_samples, max_len, n_channels) + shift
 
-    # Ground-truth distance
-    ground_truth = shift
+    # Evaluate the sliced distance with pot
+    pot_estimate = sliced_wasserstein_distance(
+        X_t=check_flat_array(dataset1),
+        X_s=check_flat_array(dataset2),
+        n_projections=num_directions,
+    )
 
     # Compute sliced wasserstein distance
     sw = SlicedWasserstein(
-        original_samples=dataset1, random_seed=random_seed, num_directions=100
+        original_samples=dataset1,
+        random_seed=random_seed,
+        num_directions=num_directions,
     )
     metrics = sw(dataset2)
 
     assert metrics["sliced_wasserstein_mean"] <= metrics["sliced_wasserstein_max"]
-    assert np.abs(metrics["sliced_wasserstein_mean"] - ground_truth) <= 0.1
+    assert np.abs(metrics["sliced_wasserstein_mean"] - pot_estimate) <= 0.1
 
 
 @pytest.mark.parametrize("shift", test_data_wasserstein)
@@ -38,8 +48,8 @@ def test_marginal_waserstein(shift: float):
     np.random.seed(random_seed)
 
     # Initialize two datasets
-    dataset1 = np.random.rand(n_samples, n_dim)
-    dataset2 = np.random.rand(n_samples, n_dim) + shift
+    dataset1 = np.random.rand(n_samples, max_len, n_channels)
+    dataset2 = np.random.rand(n_samples, max_len, n_channels) + shift
 
     # Ground-truth distance
     ground_truth = shift
