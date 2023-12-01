220b40bc45be0da8549ef969d56a6eb27b2d3993
JonathanCrabbe <jonathan.cr1302@gmail.com>
2023-12-01 13:56:34 +0000

    Initialize wandb with project and entity

--- src/fdiff/utils/wandb.py
+++ src/fdiff/utils/wandb.py
@@ -8,10 +8,7 @@ def maybe_initialize_wandb(cfg: DictConfig) -> str | None:
     """Initialize wandb if necessary."""
     cfg_flat = flatten_config(cfg)
     if "pytorch_lightning.loggers.WandbLogger" in cfg_flat.values():
-        wandb.init(
-            project="FourierDiffusion",
-            config=cfg_flat,
-        )
+        wandb.init(project="FourierDiffusion", config=cfg_flat, entity="fdiff")
         assert wandb.run is not None
         run_id = wandb.run.id
         assert isinstance(run_id, str)
