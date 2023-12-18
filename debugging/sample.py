7ed464d2886667e24f84bfca046e0d9dc41ee764
nicolas <nicolas.huynh99@gmail.com>
2023-12-18 16:03:16 +0000

    removed useless files

--- debugging/sample.py
+++ debugging/sample.py
@@ -1,5 +1,4 @@
-
-#append to the path
+# append to the path
 import sys
 
 sys.path.append("/home/nvth2/FourierDiffusion/src")
@@ -14,16 +13,18 @@ def visualize_ts(ts: torch.Tensor, title: str = "Time series") -> None:
     fig, ax = plt.subplots()
     ax.plot(ts.detach().cpu().numpy())
     ax.set_title(title)
-    #save the plot
+    # save the plot
     fig.savefig("/home/nvth2/FourierDiffusion/debugging/TimeSeries.png")
     plt.show()
-    
-    
-#Load the checkpoint
 
-score_model =  ScoreModule.load_from_checkpoint("/home/nvth2/FourierDiffusion/cmd/lightning_logs/x58vjcza/checkpoints/epoch=23-val_loss=0.02.ckpt")
+
+# Load the checkpoint
+
+score_model = ScoreModule.load_from_checkpoint(
+    "/home/nvth2/FourierDiffusion/cmd/lightning_logs/x58vjcza/checkpoints/epoch=23-val_loss=0.02.ckpt"
+)
 score_model.eval()
 sampler = TSSampler(score_model, score_model.noise_scheduler)
 ts = sampler(batch_size=2, num_inference_steps=1000)
-    
+
 visualize_ts(ts[0], title="Time series")
